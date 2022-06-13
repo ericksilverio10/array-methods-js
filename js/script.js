80ef1d3571ef6901ade2acf2cc1a25a590a069da
Erick <erick1junior@hotmail.com>
2022-06-13 19:43:00 -0300

    Método reverse

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+const data = [
+    {name: "Matheus", age: 18, sallary: 2000, driverLicense: true},
+    {name: "Erick", age: 18, sallary: 1800, driverLicense: false},
+    {name: "Cesar", age: 19, sallary: 1300, driverLicense: false},
+    {name: "Vinicius", age: 30, sallary: 1000, driverLicense: true},
+    {name: "Rosangela", age: 56, sallary: 7000, driverLicense: true},
+];
+
+// 1- Reverse
+const reverseData = data.reverse();
+
+console.log(reverseData);
