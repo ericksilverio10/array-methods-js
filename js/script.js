1d0ec459d1be8cda29df722acff1c450d29e363f
Erick <erick1junior@hotmail.com>
2022-06-13 19:59:40 -0300

    Método includes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const data = [
-    {name: "Matheus", age: 18, sallary: 2000, driverLicense: true},
+    {name: "Matheus", age: 18, sallary: 1300, driverLicense: true},
     {name: "Erick", age: 18, sallary: 5800, driverLicense: false},
     {name: "Cesar", age: 19, sallary: 3300, driverLicense: false},
     {name: "Vinicius", age: 30, sallary: 6000, driverLicense: true},
@@ -7,9 +7,9 @@ const data = [
 ];
 
 // 1- Reverse
-const reverseData = data.reverse();
+// const reverseData = data.reverse();
 
-console.log(reverseData);
+// console.log(reverseData);
 
 // 2- Find
 const highSallary = 2000;
@@ -18,3 +18,25 @@ const highestSalary = data.find((user) => user.sallary > highSallary);
 
 console.log(highestSalary);
 // retorna apenas UM resultado
+
+// 3 - FindIndex
+
+const lowestSallary = data.findIndex((user) => user.sallary > 0 && user.sallary < 2000);
+
+console.log(lowestSallary);
+
+data[lowestSallary].sallary += 200;
+
+console.log(data[lowestSallary])
+
+// 4 - Includes
+const numbers = [1, 0, 4, 2, 5, 7];
+
+const hasFour = numbers.includes(4);
+const hasSix = numbers.includes(6);
+
+console.log(hasFour);
+// true
+
+console.log(hasSix);
+// false
